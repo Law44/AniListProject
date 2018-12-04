2454b410b62461ab91f45fb1731bbdda9b3fba0e
Yago Morales <yagomoralesares@gmail.com>
2018-12-04 16:08:54 +0100

    Estructura dpm

---
+++ app/src/main/java/com/example/anilistproject/AnimeRepository.java
@@ -0,0 +1,37 @@
+package com.example.anilistproject;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AnimeRepository {
+
+    AnimeApi animeAPI;
+
+    public AnimeRepository(){
+        animeAPI = AnimeModule.getAPI();
+    }
+
+    public LiveData<List<Anime>> getMovies(){
+        final MutableLiveData<List<Anime>> lista = new MutableLiveData<>();
+
+        animeAPI.getTopAnimesRating().enqueue(new Callback<AnimesList>() {
+
+            @Override
+            public void onResponse(Call<AnimesList> call, Response<AnimesList> response) {
+                lista.setValue(response.body().results);
+            }
+
+            @Override
+            public void onFailure(Call<AnimesList> call, Throwable t) {
+            }
+        });
+
+        return lista;
+    }
+}
