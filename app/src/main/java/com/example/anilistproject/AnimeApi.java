190dbb9ab619eab00c4c9f5ec7ad77abbe975fd1
Yago Morales <yagomoralesares@gmail.com>
2018-12-04 16:03:09 +0100

    Estructura dpm

--- app/src/main/java/com/example/anilistproject/AnimeApi.java
+++ app/src/main/java/com/example/anilistproject/AnimeApi.java
@@ -1,12 +1,12 @@
 package com.example.anilistproject;
 
 import retrofit2.Call;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 
 public interface AnimeApi {
-    @POST("/3/discover/movie")
-    Call<AnimesList> getMovies();
 
-
-}
+    @GET("/top/anime/1/favorite")
+    Call<AnimesList> getTopAnimesRating();
+    }
